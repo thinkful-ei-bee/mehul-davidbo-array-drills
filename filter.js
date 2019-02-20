0eb4140ca91479104c4789e09d2522c7eccbb098
David Bolin <david@bolin.at>
2019-02-20 10:18:56 -0800

    fixed eslint problems

--- filter.js
+++ filter.js
@@ -1,14 +1,16 @@
+'use strict';
+
 function filter(arr,fn) {
-    // TASK: Define your function here
-    const newArray = [];
-    for (let i = 0; i <arr.length; i++)
+  // TASK: Define your function here
+  const newArray = [];
+  for (let i = 0; i <arr.length; i++)
+  {
+    if(fn(arr[i]))
     {
-        if(fn(arr[i]))
-        {
-            newArray.push(arr[i]);
-        }
+      newArray.push(arr[i]);
     }
-    return newArray;
+  }
+  return newArray;
 }
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
@@ -18,12 +20,12 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
 });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 console.log(filter(myNames,(name => name[0] === 'R')));
