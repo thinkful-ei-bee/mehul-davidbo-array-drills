abbe4cd6bb753597377171e7353e35fe6bb83fc7
David Bolin <david@bolin.at>
2019-02-20 09:12:46 -0800

    created max and min functions

---
+++ max-and-min.js
@@ -0,0 +1,24 @@
+'use strict';
+function max(numbers) {
+  let i = 0;
+  let currentMax = null;
+  while (i < numbers.length) {
+    if (currentMax === null || currentMax < numbers[i]) {
+      currentMax = numbers[i];
+    }
+    i++;
+  }
+  return currentMax;
+}
+
+function min(numbers) {
+  let i = 0;
+  let currentMin = null;
+  while (i < numbers.length) {
+    if (currentMin === null || currentMin > numbers[i]) {
+      currentMin = numbers[i];
+    }
+    i++;
+  }
+  return currentMin;
+}
